0ff2de77c1593d7574d6fda48538391a42a741ec
Maks Szyło <maksym.szylo@gmail.com>
2024-12-08 22:01:17 +0100

    Added password hsshing and update_user method

--- services/user_service.py
+++ services/user_service.py
@@ -1,13 +1,16 @@
 from db.models import User
 from db.db_connection import get_session
 from sqlalchemy.exc import IntegrityError
-
+from bcrypt import hashpw
+from bcrypt import gensalt
 
 class UserService:
     @staticmethod
     def create_user(username: str, password: str):
         try:
-            user = User(username=username, password=password)
+            hashed_password = hashpw(password.encode('utf-8'), gensalt())
+
+            user = User(username=username, password=hashed_password)
 
             with get_session() as db_session:
                 db_session.add(user)
@@ -36,6 +39,21 @@ class UserService:
         return users
 
     @staticmethod
+    def update_user(updated_user: User):
+        with get_session() as db_session:
+            user = UserService.get_user_by_id(updated_user.id)
+
+            if user:
+                user.username = updated_user.username
+                user.session_token = updated_user.session_token
+
+                db_session.commit()
+
+                return {"SUCCESS": f"User {user.username} was updated"}
+            else:
+                return {"ERROR": f"User doesnt exist"}
+
+    @staticmethod
     def delete_user_by_username(username: str):
         user = UserService.get_user_by_username(username)
 
@@ -45,4 +63,4 @@ class UserService:
                 db_session.commit()
             return {"SUCCESS": f"User {username} was deleted"}
         else:
-            print(f'User {username} not found!')
+            print(f'User {username} not found')
