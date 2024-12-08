22e537a8a78c32689ba13a6f1686497fb7e464aa
Maks Szyło <maksym.szylo@gmail.com>
2024-12-08 22:00:06 +0100

    Added session_token for users table and img_path for investements

--- db/models.py
+++ db/models.py
@@ -5,12 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+    session_token = Column(String(255), nullable=True)
     created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
 
     def __repr__(self):
@@ -23,6 +25,7 @@ class Investment(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     symbol = Column(String(10), unique=True, nullable=False)
+    img_path = Column(String(255), nullable=False)
 
     def __repr__(self):
         return f"Investment(id={self.id}, name={self.name}, symbol={self.symbol})"
