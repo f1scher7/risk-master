a7a60fb68041868ac716318895e9a294f09bfd75
Maks Szyło <maksym.szylo@gmail.com>
2024-12-09 23:32:26 +0100

    Added logout func

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,8 +1,7 @@
 from bcrypt import checkpw
 from uuid import uuid4
-from db.models import User
-from db.db_connection import get_session
 from services.user_service import UserService
+
 
 class AuthService:
     @staticmethod
@@ -14,9 +13,24 @@ class AuthService:
 
             UserService.update_user(updated_user=user)
 
-            return {"SUCCESS": f"User {user.username} logged in", "session_token": f"{user.session_token}"}
+            return {"SUCCESS": f"User {user.username} logged in", "username": user.username, "session_token": f"{user.session_token}"}
 
         return {"ERROR": "Invalid username or password"}
+
+    @staticmethod
+    def logout(username: str):
+        user = UserService.get_user_by_username(username)
+
+        if user and user.session_token is not None:
+            user.session_token = None
+
+            UserService.update_user(updated_user=user)
+
+            return {"SUCCESS": f"User {user.username} legged out"}
+
+        return {"ERROR": f"User {user.username} not found"}
+
+
 
     @staticmethod
     def is_password_correct(password, hashed_password):
