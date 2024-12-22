3f74f4484c4cb6f4b9ecec7165d454d4c0602b6b
Maks Szyło <maksym.szylo@gmail.com>
2024-12-22 23:49:28 +0100

    Added fixes for filter and hash_password

--- services/user_service.py
+++ services/user_service.py
@@ -5,10 +5,11 @@ from bcrypt import hashpw
 from bcrypt import gensalt
 
 class UserService:
+
     @staticmethod
     def create_user(username: str, password: str):
         try:
-            hashed_password = hashpw(password.encode('utf-8'), gensalt())
+            hashed_password = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
 
             user = User(username=username, password=hashed_password)
 
@@ -20,23 +21,27 @@ class UserService:
         except IntegrityError:
             return {"ERROR": "Username already exists"}
 
+
     @staticmethod
     def get_user_by_id(user_id: int):
         with get_session() as db_session:
-            user = db_session.query(User).filter_by(User.id == user_id).first()
+            user = db_session.query(User).filter(User.id == user_id).first()
         return user
+
 
     @staticmethod
     def get_user_by_username(username: str):
         with get_session() as db_session:
-            user = db_session.query(User).filter_by(User.username == username).first()
+            user = db_session.query(User).filter(User.username == username).first()
         return user
+
 
     @staticmethod
     def get_all_users():
         with get_session() as db_session:
             users = db_session.query(User).all()
         return users
+
 
     @staticmethod
     def update_user(updated_user: User):
@@ -52,6 +57,7 @@ class UserService:
                 return {"SUCCESS": f"User {user.username} was updated"}
             else:
                 return {"ERROR": f"User doesnt exist"}
+
 
     @staticmethod
     def delete_user_by_username(username: str):
