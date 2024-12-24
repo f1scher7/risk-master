95719351cb3e20242c6ef00ac391fe1d45c3d0d8
Maks Szyło <maksym.szylo@gmail.com>
2024-12-24 16:16:59 +0100

    Added env loader

---
+++ env_loader.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+env_path = Path(__file__).resolve().parent / ".env"
+
+load_dotenv(dotenv_path=env_path)
+
+
+RISK_MASTER_ROOT = os.getenv("RISK_MASTER_ROOT")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
