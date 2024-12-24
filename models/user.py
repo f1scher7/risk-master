c7a06d68823aee8e22404d7e5fbc3b2ca0d86960
Maks Szyło <maksym.szylo@gmail.com>
2024-12-24 16:15:57 +0100

    Added User model

---
+++ models/user.py
@@ -0,0 +1,18 @@
+import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    session_token = Column(String(255), nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.now(datetime.UTC))
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username}, created_at={self.created_at})"
