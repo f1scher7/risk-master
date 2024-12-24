4dc8598645a03434b2915ad8223d6b55f3c22096
Maks Szyło <maksym.szylo@gmail.com>
2024-12-24 16:15:04 +0100

    Added models packege and deleted db package

--- utils/db_connection_utils.py
+++ utils/db_connection_utils.py
@@ -1,20 +1,7 @@
-import os
 import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from pathlib import Path
-
-
-env_path = Path(__file__).resolve().parent.parent / ".env"
-
-load_dotenv(dotenv_path=env_path)
-
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+from env_loader import *
 
 db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
