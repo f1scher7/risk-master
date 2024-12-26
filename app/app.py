2e0e0d8db7b2ef33168d776e94f5271fe559ca5d
Maks Szyło <maksym.szylo@gmail.com>
2024-12-26 17:43:07 +0100

    Added st.config

--- app/app.py
+++ app/app.py
@@ -4,54 +4,26 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
+
 import streamlit as st
-from services.auth_service import AuthService
+from utils.session_manager_utils import initialize_session
+from screens.login_screen import login_screen
+from screens.dashboard.dashboard import dashboard_screen
 
 
-if "is_logged_in" not in st.session_state:
-    st.session_state["is_logged_in"] = False
-    st.session_state["username"] = None
-    st.session_state["session_token"] = None
+st.set_page_config(
+    page_title="RiskMaster",
+)
 
-
-def login_screen():
-    st.title("Log in")
-
-    username = st.text_input("Username")
-    password = st.text_input("Password")
-
-    print(username)
-    print(password)
-
-    if st.button("Log in"):
-        response = AuthService.login(username, password)
-
-        if "SUCCESS" in response:
-            st.success(response["SUCCESS"])
-            st.session_state["is_logged_in"] = True
-            st.session_state["username"] = response["username"]
-            st.session_state["session_token"] = response["session_token"]
-            st.experimental_rerun()
-        else:
-            st.error(response["ERROR"])
-
-def logout():
-    AuthService.logout(st.session_state["username"])
-
-    st.session_state["is_logged_in"] = False
-    st.session_state["username"] = None
-    st.session_state["session_token"] = None
-    st.experimental_rerun()
 
 def main():
+    initialize_session()
+
     if not st.session_state["is_logged_in"]:
         login_screen()
     else:
-        st.sidebar.title("Menu")
-        st.sidebar.button("Log out", on_click=logout)
+        dashboard_screen()
 
-        st.title("Witaj w aplikacji Risk-Master!")
-        st.write(f"Zalogowany jako: {st.session_state['username']}")
 
 if __name__ == "__main__":
     main()
