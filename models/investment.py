18cfbbcfb84fd3dcc6b159db0322ac2fbbf4eb3f
Maks Szyło <maksym.szylo@gmail.com>
2024-12-26 17:45:35 +0100

    Added price and percent change atributes

--- models/investment.py
+++ models/investment.py
@@ -12,5 +12,27 @@ class Investment(Base):
     symbol = Column(String(10), unique=True, nullable=False)
     img_path = Column(String(255), nullable=False)
 
+    _price = None
+    _percent_change = None
+
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def percent_change(self):
+        return self._percent_change
+
+
+    @price.setter
+    def price(self, value):
+        self._price = value
+
+    @percent_change.setter
+    def percent_change(self, value):
+        self._percent_change = value
+
+
     def __repr__(self):
-        return f"Investment(id={self.id}, name={self.name}, symbol={self.symbol})"
+        return f"Investment(id={self.id}, name={self.name}, symbol={self.symbol}, price={self.price}, percent={self.percent_change})"
