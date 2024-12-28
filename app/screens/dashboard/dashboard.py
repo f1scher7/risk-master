4f919bdb804442cfc10e9868ab079d5c7aad20fb
Maks Szyło <maksym.szylo@gmail.com>
2024-12-28 17:46:18 +0100

    Added some fixes

--- app/screens/dashboard/dashboard.py
+++ app/screens/dashboard/dashboard.py
@@ -1,15 +1,10 @@
 import streamlit as st
-from utils.session_manager_utils import logout
 from .investments_table import update_investments_table
 
 
 def dashboard_screen():
-    st.sidebar.markdown("## *Powered by FischerAI*")
-    st.sidebar.title("Menu")
-    st.sidebar.button("Log out", on_click=logout)
-
     st.title("Welcome to the RiskMaster!")
 
     table_investments_placeholder = st.empty()
-
+    
     update_investments_table(table_investments_placeholder)
