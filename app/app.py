515f09a38c3549a6caee180207c5d3d87cc471b3
Maks Szyło <maksym.szylo@gmail.com>
2024-12-28 17:49:16 +0100

    Added logic for switching screens

--- app/app.py
+++ app/app.py
@@ -6,9 +6,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 import streamlit as st
+from enums import Screen
 from utils.session_manager_utils import initialize_session
 from screens.login_screen import login_screen
+from screens.sidebar import generate_sidebar
 from screens.dashboard.dashboard import dashboard_screen
+from screens.price_prediction_screen import price_prediction_screen
+from screens.risk_analysis_screen import risk_analysis_screen
 
 
 st.set_page_config(
@@ -22,7 +26,19 @@ def main():
     if not st.session_state["is_logged_in"]:
         login_screen()
     else:
-        dashboard_screen()
+        current_screen = st.session_state["current_screen"]
+
+        generate_sidebar(current_screen == Screen.DASHBOARD)
+
+        if current_screen == Screen.DASHBOARD:
+            st.empty()
+            dashboard_screen()
+        elif current_screen == Screen.PRICE_PREDICTION:
+            st.empty()
+            price_prediction_screen()
+        elif current_screen == Screen.RISK_ANALYSIS:
+            st.empty()
+            risk_analysis_screen()
 
 
 if __name__ == "__main__":
