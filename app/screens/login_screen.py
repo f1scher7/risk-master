3829eff51e23b718931a869fa02507399238355a
Maks Szyło <maksym.szylo@gmail.com>
2024-12-28 17:44:32 +0100

    Added some fixes

--- app/screens/login_screen.py
+++ app/screens/login_screen.py
@@ -1,24 +1,24 @@
 import streamlit as st
+from enums import Screen
 from services.auth_service import AuthService
+from utils.app_utils import set_current_screen
 
 
 def login_screen():
     st.title("Log in")
 
     username = st.text_input("Username")
-    password = st.text_input("Password")
-
-    print(username)
-    print(password)
+    password = st.text_input("Password", type="password")
 
     if st.button("Log in"):
         response = AuthService.login(username, password)
 
         if "SUCCESS" in response:
-            st.success(response["SUCCESS"])
             st.session_state["is_logged_in"] = True
             st.session_state["username"] = response["username"]
             st.session_state["session_token"] = response["session_token"]
+
+            set_current_screen(Screen.DASHBOARD)
 
             st.rerun()
         else:
