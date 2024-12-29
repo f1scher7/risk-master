062354bdd172c0de450f1db99f76baf620cf35f1
Maks Szyło <maksym.szylo@gmail.com>
2024-12-29 16:45:03 +0100

    Added Columns enum

--- enums.py
+++ enums.py
@@ -5,3 +5,12 @@ class Screen(Enum):
     DASHBOARD = "dashboard"
     PRICE_PREDICTION = "price_prediction"
     RISK_ANALYSIS = "risk_analysis"
+
+
+class Columns(Enum):
+    TIMESTAMP = "Timestamp"
+    OPEN = "Open"
+    LOW = "Low"
+    HIGH = "High"
+    CLOSE = "Close"
+    VOLUME = "Volume"
