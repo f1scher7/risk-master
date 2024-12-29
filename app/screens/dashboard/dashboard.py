bd7595b2396d727343094edd80d7c307809ddeab
Maks Szyło <maksym.szylo@gmail.com>
2024-12-29 16:41:46 +0100

    Space formatting

--- app/screens/dashboard/dashboard.py
+++ app/screens/dashboard/dashboard.py
@@ -6,5 +6,5 @@ def dashboard_screen():
     st.title("Welcome to the RiskMaster!")
 
     table_investments_placeholder = st.empty()
-    
+
     update_investments_table(table_investments_placeholder)
