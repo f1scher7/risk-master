661b744d393b3988dc9dc7e56315cd43bde20c7e
Maks Szyło <maksym.szylo@gmail.com>
2024-12-29 16:46:21 +0100

    Added DATA_TRAINING_PATH and BITCOIN_DATA

--- env_loader.py
+++ env_loader.py
@@ -10,6 +10,7 @@ load_dotenv(dotenv_path=env_path)
 
 RISK_MASTER_ROOT = os.getenv("RISK_MASTER_ROOT")
 STYLES_PATH = f"{RISK_MASTER_ROOT}{os.getenv("STYLES_PATH")}"
+DATA_TRAINING_PATH =f"{RISK_MASTER_ROOT}{os.getenv("DATA_TRAINING_PATH")}"
 
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
@@ -17,3 +18,5 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+BITCOIN_DATA = os.getenv("BITCOIN_DATA")
