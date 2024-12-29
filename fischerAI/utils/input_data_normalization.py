Fixed normalization funcs for pandas DataFrame

fischerAI/utils/input_data_normalization.py
@@ -1,29 +1,26 @@
 import numpy as np
+import pandas as pd
 
 
 # We're using Min-Max normalization:
 # when we have small spread in training data (age(18, 80));
 # when we want to have a range for training data;
-def min_max_normalization(x, normalization_range, min_val=None, max_val=None):
-    if min_val is None:
-        min_val = x.min(axis=0)
-    if max_val is None:
-        max_val = x.max(axis=0)
-
-    min_range, max_range = normalization_range
-
-    # For normalization range (0, 1)
-    normalized_X = (x - min_val) / (max_val - min_val + 1e-10)
-
-    # For normalization range (a, b); for the (0, 1) normalization_X = scaled_X
-    scaled_X = normalized_X * (max_range - min_range) + min_range
-
-    return scaled_X, min_val, max_val
+def min_max_normalization(data: pd.DataFrame):
+    if (data.max() - data.min()).any() == 0:
+        print("Warning! Min-Max normalization cause division by zero")
+        return data
+    return (data - data.min()) / (data.max() - data.min())
 
 
 # We're using Log normalization:
 # when we have large numbers in training data;
 # when we have large spread in training data (price(1000$-1000000$));
-def log_normalization(x):
-    x += 1e-10 # We're adding a small value to avoid log(0)
-    return np.log(x)
+def log_normalization(data: pd.DataFrame):
+    if (data <= 0).any().any():
+        shift_value = abs(data.min().min()) + 1
+        data += shift_value
+
+        print("Warning! Data contains <=0 values")
+        print(f"Data shifted by {shift_value}")
+
+    return np.log(data + 1)