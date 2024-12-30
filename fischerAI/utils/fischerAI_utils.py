d0af0360f68187ca59e12b1f4d46855b2b85ef30
Maks Szyło <maksym.szylo@gmail.com>
2024-12-30 21:25:50 +0100

    Added plot_mse func

--- fischerAI/utils/fischerAI_utils.py
+++ fischerAI/utils/fischerAI_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from env_loader import DATA_TRAINING_PATH
 
 
@@ -8,3 +9,16 @@ def load_data_csv(file_name: str, is_training_data: bool, split_ratio: float = 0
     split_index = int(len(data) * split_ratio)
 
     return data[:split_index] if is_training_data else data[split_index:]
+
+
+def plot_mse(mse_values):
+    fig_manager = plt.get_current_fig_manager()
+    fig_manager.set_window_title('MSE')
+
+    plt.plot(mse_values, label='MSE', color='blue')
+    plt.title('Mean Squared Error over Epochs')
+    plt.ylabel('MSE')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.grid()
+    plt.show()
