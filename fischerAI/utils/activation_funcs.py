6d9879c49a8a0c2bf6e63a8bba4174183407681f
Maks Szyło <maksym.szylo@gmail.com>
2024-12-31 14:57:39 +0100

    Added tanh derivative

--- fischerAI/utils/activation_funcs.py
+++ fischerAI/utils/activation_funcs.py
@@ -9,12 +9,15 @@ def activation_derivative_func(x, func_name):
 
 
 activation_funcs = {
-    'sigmoid': lambda x: 1 / (1 + np.exp(-x)),
     'relu': lambda x: np.maximum(x, 0),
-    'tanh': lambda x: np.tanh(x)
+    'sigmoid': lambda x: 1 / (1 + np.exp(-x)),
+    'tanh': lambda x: np.tanh(x),
 }
 
 activation_derivative_funcs = {
-    'sigmoid': lambda x: x * (1 - x),
     'relu': lambda x: (x > 0).astype(float),
+    'sigmoid': lambda x: x * (1 - x),
+    'tanh': lambda x: 1 - np.tanh(x) ** 2.,
 }
+
+
