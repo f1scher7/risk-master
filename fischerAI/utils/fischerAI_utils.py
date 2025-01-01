06c5427bdef6c31047e9e29699f33b8778ea9447
Maks Szyło <maksym.szylo@gmail.com>
2025-01-01 22:28:05 +0100

    Added universal plot loss func

--- fischerAI/utils/fischerAI_utils.py
+++ fischerAI/utils/fischerAI_utils.py
@@ -11,13 +11,13 @@ def load_data_csv(file_name: str, is_training_data: bool, split_ratio: float = 0
     return data[:split_index] if is_training_data else data[split_index:]
 
 
-def plot_mse(mse_values):
+def plot_losses(loss_values, cost_func_name):
     fig_manager = plt.get_current_fig_manager()
-    fig_manager.set_window_title('MSE')
+    fig_manager.set_window_title(cost_func_name)
 
-    plt.plot(mse_values, label='MSE', color='blue')
-    plt.title('Mean Squared Error over Epochs')
-    plt.ylabel('MSE')
+    plt.plot(loss_values, label=cost_func_name, color='blue')
+    plt.title(cost_func_name)
+    plt.ylabel(cost_func_name)
     plt.xlabel('Epochs')
     plt.legend()
     plt.grid()
