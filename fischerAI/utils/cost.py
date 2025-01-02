aa6b89276bbbc94c72b7b68d853d27ba940b4b1d
Maks Szyło <maksym.szylo@gmail.com>
2025-01-02 21:07:13 +0100

    Modified cost funcs for numba.jit

--- fischerAI/utils/cost.py
+++ fischerAI/utils/cost.py
@@ -1,17 +1,22 @@
+import numba
 import numpy as np
 
 
+@numba.jit(nopython=True)
+def mse(y_true, y_pred):
+    return  np.mean(np.square(y_pred - y_true))
+
+@numba.jit(nopython=True)
+def mse_derivative(y_true, y_pred):
+    return 2. * (y_pred - y_true) / y_true.shape[0]
+
+
+@numba.jit(nopython=True)
 def cost_func(y_true, y_pred, cost_func_name):
-    return cost_funcs[cost_func_name](y_true, y_pred)
+    if cost_func_name == 'mse':
+        return mse(y_true, y_pred)
 
+@numba.jit(nopython=True)
 def cost_derivative_func(y_true, y_pred, cost_func_name):
-    return cost_derivative_funcs[cost_func_name](y_true, y_pred)
-
-
-cost_funcs = {
-    'mse': lambda y_true, y_pred: np.mean(np.square(y_pred - y_true))
-}
-
-cost_derivative_funcs = {
-    'mse': lambda y_true, y_pred: 2. * (y_pred - y_true) / y_true.shape[0]
-}
+    if cost_func_name == 'mse':
+        return mse_derivative(y_true, y_pred)
