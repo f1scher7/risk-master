6cc4ef4c070171805103680231faf1e46e45da6d
Maks Szyło <maksym.szylo@gmail.com>
2025-01-05 20:41:25 +0100

    Added load_models func and fixes for training

--- fischerAI/price_prediction/price_prediction_AI.py
+++ fischerAI/price_prediction/price_prediction_AI.py
@@ -5,19 +5,26 @@ from fischerAI.dense_nn import DenseNN
 from fischerAI.utils.cost import cost_func
 from fischerAI.utils.fischerAI_utils import plot_losses
 from fischerAI.utils.fischerAI_utils import display_training_time
+from env_loader import RISK_MASTER_ROOT, PRICE_PREDICTION_SAVED_MODELS_PATH
 
 
 class PricePredictionAI:
 
-    def __init__(self, batch_size, sequence_length, input_sequence_length, hidden_lstm_neurons, hidden_dense_neurons, epochs, learning_rate_lstm, learning_rate_dense):
+    def __init__(self, investment_symbol, batch_size, sequence_length, input_sequence_length, hidden_lstm_neurons, hidden_dense_neurons, epochs, learning_rate_lstm, learning_rate_dense, data_min_denorm, data_max_denorm):
+        self.investment_symbol = investment_symbol
+
         self.batch_size = batch_size
         self.sequence_length = sequence_length
         self.input_sequence_length = input_sequence_length
+
         self.hidden_lstm_neurons = hidden_lstm_neurons
         self.hidden_dense_neurons = hidden_dense_neurons
         self.epochs = epochs
         self.learning_rate_lstm = learning_rate_lstm
         self.learning_rate_dense = learning_rate_dense
+
+        self.data_min_denorm = data_min_denorm
+        self.data_max_denorm = data_max_denorm
 
         self.lstm = None
         self.dense_nn = None
@@ -44,10 +51,10 @@ class PricePredictionAI:
         lstm_losses = []
 
         for epoch in range(self.epochs):
-            lstm_epoch_loss = 0
-
             lstm_features = []
+            lstm_epoch_loss = 0
             inc_for_lstm = 0
+
             train_sequences_length = train_sequences.shape[0]
 
             for sequence, target_sequence in zip(train_sequences, train_target_sequences):
@@ -61,12 +68,11 @@ class PricePredictionAI:
                 lstm_epoch_loss += lstm_loss
 
                 inc_for_lstm += 1
-                if inc_for_lstm % 1000 == 0:
+                if inc_for_lstm % 100 == 0:
                     print("Sequences processed in LSTM: ", inc_for_lstm, '/', train_sequences_length)
 
 
-            lstm_features = np.array(lstm_features)
-            lstm_features_reshaped = lstm_features.reshape(self.batch_size, self.sequence_length, self.hidden_lstm_neurons)
+            lstm_features_reshaped = np.array(lstm_features).reshape(self.batch_size, self.sequence_length, self.hidden_lstm_neurons)
 
             self.dense_nn.x = lstm_features_reshaped
             self.dense_nn.y = train_target_sequences
@@ -77,13 +83,13 @@ class PricePredictionAI:
             lstm_avg_loss = lstm_epoch_loss / len(train_sequences)
             lstm_losses.append(lstm_avg_loss)
 
-            self.display_training_info(epoch, lstm_avg_loss)
+            self.display_training_info(epoch, self.epochs, lstm_avg_loss, self.dense_nn.mse_values[epoch])
 
 
         display_training_time(training_start_time)
 
-        self.lstm.save_model()
-        self.dense_nn.save_model()
+        self.lstm.save_model(lstm_losses[-1], self.investment_symbol)
+        self.dense_nn.save_model(self.dense_nn.mse_values[-1], self.investment_symbol)
 
         plot_losses(lstm_losses, 'MSE', 'LSTM')
         plot_losses(self.dense_nn.mse_values, 'MSE', 'Dense')
@@ -91,9 +97,17 @@ class PricePredictionAI:
         return lstm_losses
 
 
-    def display_training_info(self, epoch, lstm_avg_loss):
+    def display_training_info(self, epoch, epochs, lstm_avg_loss, dense_nn_loss):
         print("=" * 41)
-        print("Epoch  " + str(epoch + 1) + " / " + str(self.epochs))
+        print("Epoch  " + str(epoch + 1) + " / " + str(epochs))
         print("LSTM Loss: " + "{:.6f}".format(lstm_avg_loss))
-        print("Dense Loss: " + "{:.6f}".format(self.dense_nn.mse_values[epoch]))
+        print("Dense Loss: " + "{:.6f}".format(dense_nn_loss))
         print("=" * 41)
+
+
+    def load_models(self, lstm_file_name, dense_file_name, investment_symbol):
+        lstm_model_info = np.load(f"{RISK_MASTER_ROOT}{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol}/{lstm_file_name}", allow_pickle=True).item()
+        dense_model_info = np.load(f"{RISK_MASTER_ROOT}{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol}/{dense_file_name}", allow_pickle=True).item()
+
+        return lstm_model_info, dense_model_info
+
