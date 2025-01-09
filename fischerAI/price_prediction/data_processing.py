cdf6e053a1d123105babaaff477e3809d1a84799
Maks Szyło <maksym.szylo@gmail.com>
2025-01-09 21:38:07 +0100

    Added get_min_max_for_sequences_and_target_sequences_from_saved_models and added fixes for get_sequence

--- fischerAI/price_prediction/data_processing.py
+++ fischerAI/price_prediction/data_processing.py
@@ -4,13 +4,14 @@ import numpy as np
 from fischerAI.utils.fischerAI_utils import load_data_csv
 from fischerAI.utils.input_data_normalization import *
 from enums import Column
+from env_loader import PRICE_PREDICTION_SAVED_MODELS_PATH
 
 
-COLUMNS_FOR_TRAINING = [Column.OPEN.value, Column.HIGH.value, Column.LOW.value, Column.CLOSE.value, Column.VOLUME.value]
+COLUMNS_FOR_TRAINING = [Column.CLOSE.value]
 
 
 def prepare_data_set(file_name: str, is_training_data: bool):
-    data = load_data_csv(file_name, is_training_data)
+    data = load_data_csv(file_name, is_training_data, split_ratio=0)
 
     data[Column.TIMESTAMP.value] = pd.to_datetime(data[Column.TIMESTAMP.value], unit="s")
 
@@ -26,30 +27,40 @@ def prepare_data_set(file_name: str, is_training_data: bool):
 
     # print(len(daily_data))
 
-    daily_data[COLUMNS_FOR_TRAINING], data_min_denorm, data_max_denorm = min_max_normalization(daily_data[COLUMNS_FOR_TRAINING])
-
-    return daily_data, data_min_denorm, data_max_denorm
+    return daily_data
 
 
-def get_sequences(data, sequence_length, is_prediction=False):
+def get_sequences(data, sequence_length, sequences_min_param=None, sequences_max_param=None, target_sequences_min_param=None, target_sequences_max_param=None, is_prediction=False, is_test=False):
     data_np = data[COLUMNS_FOR_TRAINING].values
     targets_np = data[Column.CLOSE.value].values
 
     if is_prediction:
-        last_sequence_norm, _, _ = min_max_normalization(data_np[-sequence_length:])
-        return last_sequence_norm
+        last_sequence_norm = min_max_normalization_with_min_max_params(data_np[-sequence_length:], sequences_min_param, sequences_max_param)
+        return last_sequence_norm.reshape(1, last_sequence_norm.shape[0], last_sequence_norm.shape[1])
 
     sequences, target_sequences =  create_sequences_numba(data_np, targets_np.reshape(-1, 1), sequence_length)
 
-    sequences_norm, _, _, = min_max_normalization(sequences)
+    if is_test:
+        sequences_norm = min_max_normalization_with_min_max_params(sequences, sequences_min_param, sequences_max_param)
+        target_sequences_norm = min_max_normalization_with_min_max_params(target_sequences, target_sequences_min_param, target_sequences_max_param)
+        return sequences_norm, target_sequences_norm
+
+    sequences_norm, sequences_min, sequences_max, = min_max_normalization(sequences)
     target_sequences_norm, target_sequences_min, target_sequences_max, = min_max_normalization(target_sequences)
 
-    return sequences_norm, target_sequences_norm, target_sequences_min, target_sequences_max
+    return sequences_norm, sequences_min, sequences_max, target_sequences_norm, target_sequences_min, target_sequences_max
+
+
+def get_min_max_for_sequences_and_target_sequences_from_saved_models(lstm_file_name, dense_file_name, investment_symbol):
+    lstm_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol.value}/{lstm_file_name}",allow_pickle=True).item()
+    dense_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol.value}/{dense_file_name}", allow_pickle=True).item()
+
+    return lstm_model_info["sequences_min"], lstm_model_info["sequences_max"], dense_model_info["target_sequences_min"], dense_model_info["target_sequences_max"]
 
 
 @numba.jit(nopython=True, parallel=True)
 def create_sequences_numba(data_np, targets_np, sequence_length):
-    num_sequences = len(data_np) - sequence_length - sequence_length + 1
+    num_sequences = len(data_np) - 2 * sequence_length + 1
     num_features = data_np.shape[1]
 
     sequences = np.zeros((num_sequences, sequence_length, num_features))
