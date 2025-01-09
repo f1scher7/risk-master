bc4b1155dcc95941095f3a96cdc7a6e68bc5e36f
Maks Szyło <maksym.szylo@gmail.com>
2025-01-09 21:39:05 +0100

    Modified funcs for multi layers

--- fischerAI/price_prediction/price_prediction_AI.py
+++ fischerAI/price_prediction/price_prediction_AI.py
@@ -11,19 +11,21 @@ from env_loader import PRICE_PREDICTION_SAVED_MODELS_PATH
 
 class PricePredictionAI:
     
-    def __init__(self, investment_symbol, batch_size, sequence_length, input_sequence_length, hidden_lstm_neurons, hidden_dense_neurons, epochs, learning_rate_lstm, learning_rate_dense, target_sequences_min, target_sequences_max):
+    def __init__(self, investment_symbol, batch_size, sequence_length, input_sequence_length, hidden_lstm_layers, hidden_dense_layers, epochs, learning_rate_lstm, learning_rate_dense, sequences_min, sequences_max, target_sequences_min, target_sequences_max):
         self.investment_symbol = investment_symbol
 
         self.batch_size = batch_size
         self.sequence_length = sequence_length
         self.input_sequence_length = input_sequence_length
 
-        self.hidden_lstm_neurons = hidden_lstm_neurons
-        self.hidden_dense_neurons = hidden_dense_neurons
+        self.hidden_lstm_layers = hidden_lstm_layers
+        self.hidden_dense_layers = hidden_dense_layers
         self.epochs = epochs
         self.learning_rate_lstm = learning_rate_lstm
         self.learning_rate_dense = learning_rate_dense
 
+        self.sequences_min = sequences_min
+        self.sequences_max = sequences_max
         self.target_sequences_min = target_sequences_min
         self.target_sequences_max = target_sequences_max
 
@@ -33,17 +35,19 @@ class PricePredictionAI:
 
     def build_model(self):
         self.lstm = LSTM(
-            input_sequence_length=self.input_sequence_length, hidden_neurons=self.hidden_lstm_neurons,
-            epochs=self.epochs, learning_rate=self.learning_rate_lstm,
-
+            input_sequence_length=self.input_sequence_length,
+            sequences_min=self.sequences_min, sequences_max=self.sequences_max,
+            hidden_layers_neurons=self.hidden_lstm_layers,
+            epochs=self.epochs,
+            learning_rate=self.learning_rate_lstm,
         )
 
         self.dense_nn = DenseNN(
-            x=np.zeros((self.batch_size, self.sequence_length, self.hidden_lstm_neurons)), # to be updated in train()
+            x=np.zeros((self.batch_size, self.sequence_length, self.hidden_lstm_layers[-1])), # to be updated in train()
             y=np.zeros((self.batch_size, self.sequence_length, 1)), # to be updated in train()
             target_sequences_min=self.target_sequences_min,
             target_sequences_max=self.target_sequences_max,
-            hidden1_neurons=self.hidden_dense_neurons,
+            hidden_layers_neurons=self.hidden_dense_layers,
             epochs=self.epochs,
             learning_rate=self.learning_rate_dense
         )
@@ -72,17 +76,17 @@ class PricePredictionAI:
                 lstm_epoch_loss += lstm_loss
 
                 inc_for_lstm += 1
-                if inc_for_lstm % 100 == 0:
+                if inc_for_lstm % 1000 == 0:
                     print("Sequences processed in LSTM: ", inc_for_lstm, '/', train_sequences_length)
 
 
-            lstm_features_reshaped = np.array(lstm_features).reshape(self.batch_size, self.sequence_length, self.hidden_lstm_neurons)
+            lstm_features_reshaped = np.array(lstm_features).reshape(self.batch_size, self.sequence_length, self.hidden_lstm_layers[-1])
 
             self.dense_nn.x = lstm_features_reshaped
             self.dense_nn.y = train_target_sequences
 
-            hidden1_activated_arr, output_activated_arr = self.dense_nn.forward_propagation()
-            self.dense_nn.back_propagation(hidden1_activated_arr, output_activated_arr)
+            dense_nn_activations = self.dense_nn.forward_propagation()
+            self.dense_nn.back_propagation(dense_nn_activations)
 
             lstm_avg_loss = lstm_epoch_loss / len(train_sequences)
             lstm_losses.append(lstm_avg_loss)
@@ -102,7 +106,7 @@ class PricePredictionAI:
 
 
     def test(self, sequences, target_sequences, lstm_model_info, dense_model_info):
-        lstm, dense_nn = self.prepare_models_for_test_or_predict(sequences, lstm_model_info, dense_model_info, False)
+        lstm, dense_nn = self.prepare_models_for_test_or_predict(sequences, lstm_model_info, dense_model_info)
 
         lstm_features = []
         lstm_loss = 0
@@ -115,72 +119,76 @@ class PricePredictionAI:
 
             lstm_loss += cost_func(target_sequence, final_hidden_states, 'mse')
 
-        lstm_features_reshaped = np.array(lstm_features).reshape(sequences.shape[0], sequences.shape[1], lstm.hidden_neurons)
+        lstm_features_reshaped = np.array(lstm_features).reshape(sequences.shape[0], sequences.shape[1], lstm.hidden_layers_neurons[-1])
 
         dense_nn.x = lstm_features_reshaped
         dense_nn.y = target_sequences
 
-        hidden1_activated_arr, output_activated_arr = dense_nn.forward_propagation()
-        dense_nn.back_propagation(hidden1_activated_arr, output_activated_arr)
+        activations = dense_nn.forward_propagation()
+        dense_nn.back_propagation(activations)
 
         display_training_info(0, 1, lstm_loss / len(target_sequences), dense_nn.mse_values[0])
 
 
     def predict_for_n_days(self, sequence, lstm_model_info, dense_model_info, n_days):
-        lstm, dense_nn = self.prepare_models_for_test_or_predict(sequence, lstm_model_info, dense_model_info, True)
+        lstm, dense_nn = self.prepare_models_for_test_or_predict(sequence, lstm_model_info, dense_model_info)
 
         lstm_features = []
 
-        final_hidden_states, _ = lstm.forward_propagation(sequence)
+        sequence_reshaped = sequence.reshape(sequence.shape[1], sequence.shape[2])
+
+        final_hidden_states, _ = lstm.forward_propagation(sequence_reshaped)
         lstm_features.append(final_hidden_states)
 
-        lstm_features_reshaped = np.array(lstm_features).reshape(self.batch_size, sequence.shape[0], lstm.hidden_neurons)
+        lstm_features_reshaped = np.array(lstm_features).reshape(sequence.shape[0], sequence.shape[1], lstm.hidden_layers_neurons[-1])
 
         dense_nn.x = lstm_features_reshaped
-        _, output_activated_arr = dense_nn.forward_propagation()
+        dense_nn.batch_size = sequence.shape[0]
+        dense_nn.target_sequence_length = sequence.shape[1]
 
-        print(output_activated_arr.shape)
+        activations = dense_nn.forward_propagation()
+        output_activated_arr = activations[-1]
+
         denorm_output_activated_arr = min_max_denormalization(output_activated_arr, dense_nn.target_sequences_min, dense_nn.target_sequences_max)
 
-        return denorm_output_activated_arr[:, :n_days, :]
+        return denorm_output_activated_arr[:n_days]
 
 
-    def prepare_models_for_test_or_predict(self, sequences, lstm_model_info, dense_model_info, is_predict):
+    def prepare_models_for_test_or_predict(self, sequences, lstm_model_info, dense_model_info):
         lstm = LSTM(
-            input_sequence_length=sequences.shape[1] if is_predict else sequences.shape[2],
-            hidden_neurons=lstm_model_info["hidden_neurons"],
+            input_sequence_length=sequences.shape[2],
+            sequences_min=lstm_model_info["sequences_min"],
+            sequences_max=lstm_model_info["sequences_max"],
+            hidden_layers_neurons=lstm_model_info["hidden_layers_neurons"],
         )
 
-        lstm.forget_gate_weights = lstm_model_info["forget_gate_weights"]
-        lstm.input_gate_weights = lstm_model_info["input_gate_weights"]
-        lstm.candidate_gate_weights = lstm_model_info["candidate_gate_weights"]
-        lstm.output_gate_weights = lstm_model_info["output_gate_weights"]
-
-        lstm.forget_gate_bias = lstm_model_info["forget_gate_bias"]
-        lstm.input_gate_bias = lstm_model_info["input_gate_bias"]
-        lstm.candidate_gate_bias = lstm_model_info["candidate_gate_bias"]
-        lstm.output_gate_bias = lstm_model_info["output_gate_bias"]
+        for i, layer_info in enumerate(lstm_model_info["layers"]):
+            lstm.layers[i]['forget_gate_weights'] = layer_info["forget_gate_weights"]
+            lstm.layers[i]['input_gate_weights'] = layer_info["input_gate_weights"]
+            lstm.layers[i]['candidate_gate_weights'] = layer_info["candidate_gate_weights"]
+            lstm.layers[i]['output_gate_weights'] = layer_info["output_gate_weights"]
+            lstm.layers[i]['forget_gate_bias'] = layer_info["forget_gate_bias"]
+            lstm.layers[i]['input_gate_bias'] = layer_info["input_gate_bias"]
+            lstm.layers[i]['candidate_gate_bias'] = layer_info["candidate_gate_bias"]
+            lstm.layers[i]['output_gate_bias'] = layer_info["output_gate_bias"]
 
         dense_nn = DenseNN(
-            x=np.zeros((sequences.shape[0], sequences.shape[1], self.hidden_lstm_neurons)),
+            x=np.zeros((sequences.shape[0], sequences.shape[1], lstm_model_info["hidden_layers_neurons"][-1])),
             y=np.zeros((sequences.shape[0], sequences.shape[1], 1)),
             target_sequences_min=dense_model_info["target_sequences_min"],
             target_sequences_max=dense_model_info["target_sequences_max"],
-            hidden1_neurons=dense_model_info["hidden1_neurons"],
+            hidden_layers_neurons=dense_model_info["hidden_layers_neurons"],
         )
 
-        dense_nn.input_to_hidden1_weights = dense_model_info["input_to_hidden1_weights"]
-        dense_nn.hidden1_to_output_weights = dense_model_info["hidden1_to_output_weights"]
-
-        dense_nn.hidden1_bias = dense_model_info["hidden1_bias"]
-        dense_nn.output_bias = dense_model_info["output_bias"]
+        dense_nn.weights = dense_model_info["weights"]
+        dense_nn.biases = dense_model_info["biases"]
 
         return lstm, dense_nn
 
 
     def load_models(self, lstm_file_name, dense_file_name):
-        lstm_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{self.investment_symbol}/{lstm_file_name}", allow_pickle=True).item()
-        dense_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{self.investment_symbol}/{dense_file_name}", allow_pickle=True).item()
+        lstm_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{self.investment_symbol.value}/{lstm_file_name}", allow_pickle=True).item()
+        dense_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{self.investment_symbol.value}/{dense_file_name}", allow_pickle=True).item()
 
         return lstm_model_info, dense_model_info
 
