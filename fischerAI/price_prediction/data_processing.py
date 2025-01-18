0d890cd7a8789c491fe9eeeee1b1f37c8632d7bd
Maks Szyło <maksym.szylo@gmail.com>
2025-01-18 16:01:34 +0100

    Changed prepare_data_set func for different data sets

--- fischerAI/price_prediction/data_processing.py
+++ fischerAI/price_prediction/data_processing.py
@@ -5,7 +5,7 @@ from fischerAI.utils.fischerAI_utils import load_data_csv
 from fischerAI.utils.input_data_normalization import *
 from enums import Column
 from env_loader import PRICE_PREDICTION_SAVED_MODELS_PATH
-
+from enums import InvestmentSymbol
 
 COLUMNS_FOR_TRAINING = [Column.CLOSE.value]
 
@@ -13,24 +13,34 @@ COLUMNS_FOR_TRAINING = [Column.CLOSE.value]
 def prepare_data_set(file_name: str, is_training_data: bool):
     data = load_data_csv(file_name, is_training_data, split_ratio=0)
 
-    data[Column.TIMESTAMP.value] = pd.to_datetime(data[Column.TIMESTAMP.value], unit="s")
+    if InvestmentSymbol.BITCOIN.value in file_name:
+        data[Column.TIMESTAMP.value] = pd.to_datetime(data[Column.TIMESTAMP.value], unit="s")
 
-    data.set_index(data[Column.TIMESTAMP.value], inplace=True)
-    data.dropna(inplace=True)
+        data.set_index(data[Column.TIMESTAMP.value], inplace=True)
+        data.dropna(inplace=True)
 
-    # print("Data before normalization:")
-    # print(data.tail(100))
+        data['date'] = data.index.date
 
-    data['date'] = data.index.date
+        daily_data = data.groupby('date').apply(lambda x: x.iloc[[-1]])
+    else:
+        if InvestmentSymbol.GOLD.value in file_name:
+            data['Date'] = pd.to_datetime(data['Date'], format="%b %d, %Y")
+        elif InvestmentSymbol.SILVER.value in file_name or InvestmentSymbol.ETHEREUM.value in file_name:
+            data['Date'] = pd.to_datetime(data['Date'], format="%Y-%m-%d")
+            data = data.iloc[::-1]
 
-    daily_data = data.groupby('date').apply(lambda x: x.iloc[[-1]])
+        data.set_index('Date', inplace=True)
+        data.dropna(inplace=True)
 
-    # print(len(daily_data))
+        data['date'] = data.index.date
+
+        daily_data = data.groupby('date').apply(lambda x: x.iloc[[-1]])
+        daily_data.reset_index(drop=True, inplace=True)
 
     return daily_data
 
 
-def get_sequences(data, sequence_length, sequences_min_param=None, sequences_max_param=None, target_sequences_min_param=None, target_sequences_max_param=None, is_prediction=False, is_test=False, is_graph=False):
+def  get_sequences(data, sequence_length, sequences_min_param=None, sequences_max_param=None, target_sequences_min_param=None, target_sequences_max_param=None, is_prediction=False, is_test=False, is_graph=False):
     data_np = data[COLUMNS_FOR_TRAINING].values
     targets_np = data[Column.CLOSE.value].values
 
@@ -56,9 +66,18 @@ def get_sequences(data, sequence_length, sequences_min_param=None, sequences_max
 
 def get_min_max_for_sequences_and_target_sequences_from_saved_models(lstm_file_name, dense_file_name, investment_symbol):
     if lstm_file_name is None and dense_file_name is None:
-        if investment_symbol == 'btc':
+        if investment_symbol == 'gold':
+            lstm_file_name = 'multi_layer_lstm_model_20250115-192715.npy'
+            dense_file_name = 'dense_nn_model_20250115-192715.npy'
+        elif investment_symbol == 'btc':
             lstm_file_name = 'multi_layer_lstm_model_20250108-235552_best.npy'
             dense_file_name = 'dense_nn_model_20250108-235552_best.npy'
+        elif investment_symbol == 'silver':
+            lstm_file_name = 'multi_layer_lstm_model_20250116-111026.npy'
+            dense_file_name = 'dense_nn_model_20250116-111026.npy'
+        elif investment_symbol == 'eth':
+            lstm_file_name = 'multi_layer_lstm_model_20250115-233430.npy'
+            dense_file_name = 'dense_nn_model_20250115-233430.npy'
 
     lstm_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol}/{lstm_file_name}",allow_pickle=True).item()
     dense_model_info = np.load(f"{PRICE_PREDICTION_SAVED_MODELS_PATH}{investment_symbol}/{dense_file_name}", allow_pickle=True).item()
