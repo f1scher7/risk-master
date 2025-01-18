e9a9302912065657195f2e4b770d98595adbae65
Maks Szyło <maksym.szylo@gmail.com>
2025-01-18 16:05:18 +0100

    Added some fixes

--- app/screens/sidebar.py
+++ app/screens/sidebar.py
@@ -12,7 +12,7 @@ def generate_sidebar(is_dashboard):
     if st.sidebar.button("Price Prediction", use_container_width=True):
         set_current_screen(Screen.PRICE_PREDICTION)
 
-    if st.sidebar.button("Risk Analysis", use_container_width=True):
+    if st.sidebar.button("Risk Analysis (In progress)", use_container_width=True):
         set_current_screen(Screen.RISK_ANALYSIS)
 
     if not is_dashboard:
